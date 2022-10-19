1f4bb34a02de327f02e9f52617d0ef33a3c6f336
Arsanaibar <arsan05062003@gmail.com>
2022-10-19 19:49:14 +0800

    first init

---
+++ web portofolio/filejavascript.js
@@ -0,0 +1,20 @@
+document.getElementById('form').addEventListener('submit',(event)=>
+{
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+    event.preventDefault()
+
+    let usernamenya = "arsan"
+    let passwordnya = "arsan"
+
+        if (usernamenya == username && passwordnya == password){
+            alert('selamat datang')
+            window.location.href = "index.html"
+
+        } else {
+            alert('username atau password anda salah!')
+
+        }
+    
+
+})
